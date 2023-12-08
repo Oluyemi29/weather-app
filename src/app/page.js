07aa8627518ecca3558f910e054b887c82ad71f7
Oluyemi29 <adedokunoluyemi29@gmail.com>
2023-12-08 23:11:53 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,133 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react';
+import { CiCloudOn } from "react-icons/ci";
 
 export default function Home() {
+  const [datum, setDatum]=useState({})
+  const [formData,setFormData]=useState({
+    location : ''
+  })
+  
+  const handleChange =(e)=>{
+    const{name,value}=e.target
+    setFormData((previousData)=>{
+      return{
+        ...previousData,
+        [name]:value
+      }
+    })
+  }
+  
+  const handleSearch = async (e)=>{
+    e.preventDefault()
+    const {location} = formData
+    const api_key = 'd87dba1411494061b8632600230712'
+    const api_url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${location}`
+    const res = await fetch(api_url)
+    const data = await res.json()
+    setDatum(data)
+    console.log(data)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='h-screen mt-2 md:m-5 md:flex md:flex-row md:gap-5'>
+      <div className='bg-[#7c67f5] md:w-3/12 w-full rounded-2xl text-sm text-center text-white p-5'>
+        <h3 className='text-2xl font-bold mt-10 mb-4'>WEATHER APP</h3>
+        <p>get the current weather details of any location</p>
+        <CiCloudOn  className='w-48 h-48 m-auto border-1 border-white'/> 
+        <form>
+          <input type='text' placeholder='Type Location' className='w-full h-10 pl-4 text-black border-1 border-[#7c67f5] rounded-md mb-5'name='location' value={formData.location} onChange={handleChange}/>
+          <button onClick={handleSearch} className='w-full h-10 border-2 border-white rounded-md'>Search</button>
+        </form>
+            <h4 className='mt-6 mb-3'>Temperature Ranges</h4>
+        <div className='w-full flex text-[0.7rem] justify-between'>
+        <div>
+            <p>Raining</p>
+            <p>0°C - 10°C</p>
+          </div>
+          <div>
+            <p>SunShine</p>
+            <p>Greater than 10°C</p>
+          </div>
+          <div>
+            <p>Cold or Snow</p>
+            <p>Less than 0°C</p>
+          </div>
         </div>
       </div>
+      <div className='md:w-9/12 w-full rounded-2xl text-center'>
+        <div className='flex flex-row justify-between mb-4 mt-5 md:mt-0'>
+          <h3 className='text-[0.7rem] text-[#7c67f5]'>Last updated : <br/> {datum?.current?.last_updated}</h3>
+          <h3 className='text-xl text-[#7c67f5] font-bold'>CURRENT</h3>
+          <h3 className='text-[0.7rem] text-[#7c67f5]'>Last updated epoch :<br/> {datum?.current?.last_updated_epoch}</h3>
+        </div>
+        <div className='w-full md:flex md:gap-5 gap-2 md:flex-row justify-between'>
+          <div className='w-full mb-3 md:h-44 h-36 text-white flex flex-col justify-center bg-[#7c67f5] rounded-md'>
+            <h3 className='text-sm'>Wind Degree</h3>
+            <h3 className='font-bold text-xl'>{datum?.current?.wind_degree}</h3>
+            <h3 className='text-[0.7rem] mt-2'>Wind Direction</h3>
+            <h3 className='font-bold text-[0.7rem]'>{datum?.current?.wind_dir}</h3>
+          </div>
+          <div className='w-full mb-3 h-36 md:h-44 text-white flex flex-col justify-center bg-[#7c67f5] rounded-md'>
+            <h3 className='text-sm'>Temperature C</h3>
+            <h3 className='font-bold text-xl'>{datum?.current?.temp_c}</h3>
+            <h3 className='text-[0.7rem] mt-2'>Temperature F</h3>
+            <h3 className='font-bold text-[0.7rem]'>{datum?.current?.temp_f}</h3>
+          </div>
+          <div className='w-full md:h-44 h-36 text-white flex flex-col justify-center bg-[#7c67f5] rounded-md'>
+            <h3 className='text-[0.9rem]'>Wind KPH</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.wind_kph}</h3>
+            <h3 className='text-[0.9rem]'>Wind MPH</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.wind_mph}</h3>
+          </div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <div className='w-full grid grid-cols-2 md:flex md:gap-5 gap-3 mt-5 md:flex-row justify-between'>
+          <div className='cards w-full md:h-44 h-32 text-[#7c67f5] flex flex-col justify-center rounded-md'>
+            <h3 className='text-[0.9rem]'>Feelslike C</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.feelslike_c}</h3>
+            <h3 className='text-[0.9rem]'>Feelslike F</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.feelslike_f}</h3>
+          </div>
+          <div className='cards w-full md:h-44 h-32 text-[#7c67f5] flex flex-col justify-center rounded-md'>
+            <h3 className='text-[0.9rem]'>Gust KPH</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.gust_kph}</h3>
+            <h3 className='text-[0.9rem]'>Gust MPH</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.gust_mph}</h3>
+          </div>
+          <div className='cards w-full md:h-44 h-32 text-[#7c67f5] flex flex-col justify-center rounded-md'>
+            <h3 className='text-[0.9rem] mb-0'>{datum?.current?.condition?.text}</h3>
+            {datum?.current?.condition?.icon?<img src={`https:${datum?.current?.condition?.icon}`} className='md:w-32 w-20 h-20 md:h-32 m-auto' />:''}
+            <h3 className='text-[0.9rem]'>Cloud : {datum?.current?.cloud}</h3>
+          </div>
+          <div className='cards w-full md:h-44 h-32 text-[#7c67f5] flex flex-col justify-center rounded-md'>
+            <h3 className='text-[0.9rem]'>Pressure In</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.pressure_in}</h3>
+            <h3 className='text-[0.9rem]'>Pressure MB</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.current?.pressure_mb}</h3>
+          </div>
+        </div>
+        <div className='w-full md:flex md:gap-5 gap-2 md:flex-row justify-between mt-5'>
+          <div className='w-full md:h-44 h-36 mb-3 text-white flex flex-col justify-center bg-[#7c67f5] rounded-md'>
+            <h3 className='text-sm'>Country</h3>
+            <h3 className='font-bold text-xl'>{datum?.location?.country}</h3>
+            <h3 className='text-[0.7rem] mt-2'>Tz ID</h3>
+            <h3 className='font-bold text-[0.7rem]'>{datum?.location?.tz_id}</h3>
+          </div>
+          <div className='w-full md:h-44 h-36 mb-3 text-white flex flex-col justify-center bg-[#7c67f5] rounded-md'>
+            <h3 className='text-sm'>Latitude</h3>
+            <h3 className='font-bold text-xl'>{datum?.location?.lat}</h3>
+            <h3 className='text-[0.7rem] mt-2'>Longitude</h3>
+            <h3 className='font-bold text-[0.7rem]'>{datum?.location?.lon}</h3>
+          </div>
+          <div className='w-full md:h-44 h-36 mb-3 text-white flex flex-col justify-center bg-[#7c67f5] rounded-md'>
+            <h3 className='text-[0.9rem]'>Name</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.location?.name}</h3>
+            <h3 className='text-[0.9rem]'>Region</h3>
+            <h3 className='font-bold text-[0.9rem]'>{datum?.location?.region}</h3>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
